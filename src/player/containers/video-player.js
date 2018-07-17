1d6927bd63cb25330ab807be9f99d1d26d88ad7e
Carlos Andres Rodriguez <carlosdumar4@gmail.com>
2018-07-17 16:55:52 -0500

    fourth_2 commit

---
+++ src/player/containers/video-player.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import VideoPlayerLayout from ''
+
+class VideoPlayer extends Component {
+    render() {
+        return (
+            <VideoPlayerLayout>
+
+            </VideoPlayerLayout>
+        )
+    }
+}
+
+export default VideoPlayer;
