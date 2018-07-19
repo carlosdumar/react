4be6f829857e2c2aba3036059e036a6f428f0be1
Carlos Andres Rodriguez <carlosdumar4@gmail.com>
2018-07-19 16:57:56 -0500

    Sixth commit

--- src/player/components/video.js
+++ src/player/components/video.js
@@ -2,12 +2,31 @@ import React, { Component } from 'react';
 import './video.css';
 
 class Video extends Component {
+    togglePlay() {
+        this.props.pause ? this.video.play() : this.video.pause();
+    }
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.pause !== this.props.pause) {
+            this.togglePlay()
+        }
+    }
+    setRef = element => {
+        this.video = element;
+    }
     render() {
+        const {
+            handleLoadedMetadata,
+            handleTimeUpdate
+        } = this.props;
+
         return (
             <div className="Video">
                 <video
                     autoPlay={this.props.autoplay}
                     src={this.props.src}
+                    ref={this.setRef}
+                    onLoadedMetadata={handleLoadedMetadata}
+                    onTimeUpdate={handleTimeUpdate}
                 />
             </div>
         )
